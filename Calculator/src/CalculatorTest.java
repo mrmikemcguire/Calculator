6c101ec86480d363e3c416e0f7fd2bd14b0aaab4
Mike McGuire <mike.mcguire@mullenhigh.com>
2023-03-07 13:13:09 -0700

    unsuccessful changes

--- Calculator/src/CalculatorTest.java
+++ Calculator/src/CalculatorTest.java
@@ -7,29 +7,29 @@ public class CalculatorTest
 		@Test
 		public void testAdd()
 			{
-				assertEquals(5, Calculator.add(2, 3));
-				fail("Not yet implemented");
+			assertEquals(5, Calculator.add(2, 3));
+			fail("Not yet implemented");
 			}
 
 		@Test
 		public void testSubtract()
 			{
-				assertEquals(2, Calculator.subtract(5, 3));
-				fail("Not yet implemented");
+			assertEquals(2, Calculator.subtract(5, 3));
+			fail("Not yet implemented");
 			}
 
 		@Test
 		public void testMultiply()
 			{
-				assertEquals(6, Calculator.multiply(2, 3));
-				fail("Not yet implemented");
+			assertEquals(6, Calculator.multiply(2, 3));
+			fail("Not yet implemented");
 			}
 
 		@Test
 		public void testDivide()
 			{
-				assertEquals(4, Calculator.add(20, 5));
-				fail("Not yet implemented");
+			assertEquals(4, Calculator.divide(20, 5));
+			fail("Not yet implemented");
 			}
 
 	}
