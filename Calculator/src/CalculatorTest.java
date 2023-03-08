75922301ce13bc651abe91be0b80eb5071afe6d0
Mike McGuire <mike.mcguire@mullenhigh.com>
2023-03-08 06:49:12 -0700

    all tests working

--- Calculator/src/CalculatorTest.java
+++ Calculator/src/CalculatorTest.java
@@ -8,28 +8,23 @@ public class CalculatorTest
 		public void testAdd()
 			{
 			assertEquals(5, Calculator.add(2, 3));
-			fail("Not yet implemented");
 			}
 
 		@Test
 		public void testSubtract()
 			{
 			assertEquals(2, Calculator.subtract(5, 3));
-			fail("Not yet implemented");
 			}
 
 		@Test
 		public void testMultiply()
 			{
 			assertEquals(6, Calculator.multiply(2, 3));
-			fail("Not yet implemented");
 			}
 
 		@Test
 		public void testDivide()
 			{
 			assertEquals(4, Calculator.divide(20, 5));
-			fail("Not yet implemented");
 			}
-
 	}
